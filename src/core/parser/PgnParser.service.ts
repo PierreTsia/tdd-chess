32f31fef606b7233c101c9048be9ffc9b1819b2c
PierreTsia <pierre.tsiakkaros@gmail.com>
2021-07-20 00:32:36 +0200

    feat(pgn-parser): validation of entire pgn sequence

--- src/core/parser/PgnParser.service.ts
+++ src/core/parser/PgnParser.service.ts
@@ -1,23 +1,41 @@
+const NUMBER_FOLLOWED_BY_DOT: RegExp = /^\d+(\.+)$/;
+const VALID_CHESS_MOVE: RegExp = /(?:[PNBRQK]?[a-h]?[1-8]?x?[a-h][1-8](?:\=[PNBRQK])?|O(-?O){1,2})[\+#]?(\s*[\!\?]+)?/;
+const VALID_END_OF_GAME = ['1/2-1/2', '1-0', '0-1'];
+
 export class PgnParserService {
-  private NUMBER_FOLLOWED_BY_DOT: RegExp = /^\d+(\.+)$/;
   public parse(pgn: string): any[] {
-    return pgn.split(' ').reduce((acc: string[][], sequence, sequenceIndex, array) => {
-      if (this.isACountNumber(sequence)) {
-        const moveEndIndex = this.findEndOfMoveIndex(array, sequenceIndex);
-        const move = moveEndIndex === -1 ? array.slice(sequenceIndex) : array.slice(sequenceIndex, moveEndIndex);
-        acc.push(move);
+    return pgn.split(' ').reduce((acc: string[][], str, turnIndex, array) => {
+      if (turnIndex === 0 && str !== '1.') {
+        throw new Error(`Must start by first turn`);
+      } else if (!this.isACountNumber(str) && this.isNotValid(str)) {
+        throw new Error(`Invalid move ${str}`);
+      } else if (this.isACountNumber(str)) {
+        const endIndex = this.findEndOfTurnIndex(array, turnIndex);
+        const turn = endIndex === -1 ? array.slice(turnIndex) : array.slice(turnIndex, endIndex);
+        acc.push(turn);
       }
 
       return acc;
     }, []);
-
   }
 
-  private findEndOfMoveIndex(array: string[], sequenceIndex: number) {
-    return array.findIndex((seq, i) => i > sequenceIndex && this.isACountNumber(seq));
+  private isNotValid(turn: string) {
+    return !turn.split(' ').every(s => this.isAValidMove(s) || this.isWinnerDeclared(s));
+  }
+
+  private isAValidMove(move: string): boolean {
+    return VALID_CHESS_MOVE.test(move);
+  }
+
+  private isWinnerDeclared(str: string) {
+    return VALID_END_OF_GAME.includes(str);
+  }
+
+  private findEndOfTurnIndex(array: string[], startIndex: number) {
+    return array.findIndex((turn, i) => i > startIndex && this.isACountNumber(turn));
   }
 
   private isACountNumber(sequence: string) {
-    return this.NUMBER_FOLLOWED_BY_DOT.test(sequence);
+    return NUMBER_FOLLOWED_BY_DOT.test(sequence);
   }
 }
